6a91d326ff0af9d23cbf147d903abac97f062d0f
MARTINA-TORCE <MARTINA.TORCE20@IMPERIAL.AC.UK>
2023-01-15 15:14:28 +0000

    clean up files

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore } from "vuex";
-import exchange from "./modules/exchange";
 import user from "./modules/user";
 import toast from "./modules/toast";
 import paper from "./modules/paper";
@@ -8,7 +7,6 @@ import project from "./modules/project";
 // Vuex built-in method to create a new store, split into different modules
 export default createStore({
   modules: {
-    exchange,
     user,
     toast,
     paper,
